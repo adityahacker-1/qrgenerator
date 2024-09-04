ba754871e3a21bde65824075300bdf5047d202f2
aditya_baniya <123892116+adityahacker-1@users.noreply.github.com>
2024-09-04 00:26:49 -0500

    Update qrcode2.py

--- qrcode2.py
+++ qrcode2.py
@@ -4,7 +4,9 @@ qr = qrcode.QRCode(border=4,
                    box_size=10,
                    version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H)
-qr.add_data('I am aditya')
+message = input("Enter a message: ")
+qr.add_data(message)
 qr.make(fit=True)
 img = qr.make_image(fill_color='black', back_color="white")
-img.save("adi.png")
+image_address = input("Enter a file name: ")
+img.save(image_address)
